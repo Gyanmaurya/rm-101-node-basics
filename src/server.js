7fd23b3b210b586574aa30b2648bf3121bcee1dc
Gyanmaurya <gyanprakashme0051@gmail.com>
2022-06-20 19:55:43 +0530

    final

--- src/server.js
+++ src/server.js
@@ -1,8 +1,24 @@
 // install and import express
-const express = () => {};
+const express = require("express");
+const path = require("path");
 let app = express();
+
 
 // Code here
 
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "../../src/assets/users.html"));
+});
+
+app.get("/users",(req, res) => {
+    // console.log("user")
+  res.sendFile(path.join(__dirname + "../../src/assets/user.json"));
+});
+
+app.listen(8000, () => {
+    // console.log("server is running at port :",port)
+})
 // Note: Do not remove this export statement
 module.exports = app;
